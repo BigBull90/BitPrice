3f9541252c9bc7b6b50b2a1db11aba430f28575a
Harry Singh <me@harryio.com>
2017-12-29 17:36:52 +0530

    Add basic network code

---
+++ app/src/main/java/com/harryio/bitprice/net/ServiceFactory.java
@@ -0,0 +1,27 @@
+package com.harryio.bitprice.net;
+
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceFactory {
+
+    private static BitPriceService bitPriceService;
+
+    private ServiceFactory() {
+    }
+
+    public static BitPriceService getService() {
+        if (bitPriceService == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .addCallAdapterFactory(
+                            RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            bitPriceService = retrofit.create(BitPriceService.class);
+        }
+
+        return bitPriceService;
+    }
+}
