00d044091d2ce3f2c09c58eea02853581735b56c
Harry Singh <me@harryio.com>
2017-12-29 17:40:28 +0530

    Add api interactor type

--- app/src/main/java/com/harryio/bitprice/net/ServiceFactory.java
+++ app/src/main/java/com/harryio/bitprice/net/ServiceFactory.java
@@ -5,7 +5,7 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ServiceFactory {
+public final class ServiceFactory {
 
     private static BitPriceService bitPriceService;
 
