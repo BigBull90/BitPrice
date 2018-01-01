3badf24ad2aa44e682a93ac1d431f2716df36847
Harry Singh <me@harryio.com>
2018-01-01 21:50:32 +0530

    Add logging interceptor

--- app/src/main/java/com/harryio/bitprice/net/ServiceFactory.java
+++ app/src/main/java/com/harryio/bitprice/net/ServiceFactory.java
@@ -2,6 +2,8 @@ package com.harryio.bitprice.net;
 
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -15,7 +17,12 @@ final class ServiceFactory {
 
     static BitPriceService getService() {
         if (bitPriceService == null) {
-            OkHttpClient okHttpClient = new OkHttpClient();
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(Level.BODY);
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .build();
+            
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl("http://myapi.com/")
                     .client(okHttpClient)
